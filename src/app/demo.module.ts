1df2109b7686f2ccb4e98807851d74ca0b5245c4
alek <alekart@users.noreply.github.com>
2019-12-22 19:14:19 +0100

    Remove integrated frameworks to decrease peer dependencies

--- src/app/demo.module.ts
+++ src/app/demo.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -11,9 +12,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
-
-
 
 // To include JsonSchemaFormModule after downloading from NPM, use this instead:
 //
@@ -29,21 +27,16 @@ import { DemoComponent } from './demo.component';
 import { DemoRootComponent } from './demo-root.component';
 
 import { routes } from './demo.routes';
-import {
-  MaterialDesignFrameworkModule, Bootstrap4FrameworkModule,
-  Bootstrap3FrameworkModule, NoFrameworkModule
-} from '@alekart/angular-json-schema-form';
+import {JsonSchemaFormModule, NoFrameworkModule} from '@alekart/angular-json-schema-form';
 
 @NgModule({
   declarations: [AceEditorDirective, DemoComponent, DemoRootComponent],
   imports: [
-    BrowserModule, BrowserAnimationsModule, FlexLayoutModule, FormsModule,
-    HttpClientModule, MatButtonModule, MatCardModule, MatCheckboxModule,
+    BrowserModule, BrowserAnimationsModule, FormsModule, HttpClientModule,
+    MatButtonModule, MatCardModule, MatCheckboxModule, FlexLayoutModule,
     MatIconModule, MatMenuModule, MatSelectModule, MatToolbarModule,
     RouterModule.forRoot(routes),
-    MaterialDesignFrameworkModule,
-    Bootstrap4FrameworkModule,
-    Bootstrap3FrameworkModule,
+    JsonSchemaFormModule,
     NoFrameworkModule
   ],
   bootstrap: [DemoRootComponent]
